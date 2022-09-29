16cc5288715596eca0c1385ea063b55fd7c116e9
Ibrahim Velizade <ivalizada14@gmail.com>
2022-09-29 22:29:07 +0300

    first try

--- src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
@@ -2,5 +2,10 @@ package lang.print.gaps.task6;
 
 public class FloatTypeCasting {
     public void roundNumber(float numberToBeRounded) {
+        float afterDot = numberToBeRounded - (long)numberToBeRounded;
+        long roundedValue = (long)numberToBeRounded;
+        if(afterDot >= 0.5)
+            roundedValue++;
+        System.out.println(roundedValue);
     }
 }
